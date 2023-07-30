c5fe27f93557c5fbb7833d4e9d87230b3b93a9fa
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-07-30 23:37:58 +0200

    - Implemented Part Two of day 03 + unit tests; - Improved unit test for Day 03, Part One - Simplified printout for the 3 days

--- 2015/day_03_2015.py
+++ 2015/day_03_2015.py
@@ -19,8 +19,45 @@ For example:
 ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
 
 
+--- Part Two ---
+
+The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents
+with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns
+moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
 
 """
+
+
+def one_move(x: int, y: int, char: str):
+    """
+    Update latitude (X) or longitude (Y) depending on the move character.
+
+    :param x: starting latitude
+    :param y: starting longitude
+    :param move: move character. Supported: north (^), south (v), east (>), west (<)
+    :return: a tuple with updated x and y coordinates
+    """
+    if char == "^":
+        y += 1
+    elif char == ">":
+        x += 1
+    elif char == "<":
+        x -= 1
+    elif char == "v":
+        y -= 1
+    else:
+        Exception(f"Invalid character in move instructions: {char}")
+    return x, y
 
 
 def deliver_presents(move_instructions: str):
@@ -37,20 +74,58 @@ def deliver_presents(move_instructions: str):
     y = 0
     char = "."
     presents_per_house[(x, y)] = 1
-    # print(f'\t{char}\t{x}\t{y}\t{presents_per_house[(x, y)]}\n')
+    # print(f'\t{char}\t{x}\t{y}\t{presents_per_house[(x, y)]}')
     for char in move_instructions:
-        if char == "^":
-            y += 1
-        elif char == ">":
-            x += 1
-        elif char == "<":
-            x -= 1
-        elif char == "v":
-            y -= 1
-        else:
-            Exception(f"Invalid character in move instructions: {char}")
-
+        x, y = one_move(x, y, char)
         presents_per_house[(x, y)] = presents_per_house.get((x, y), 0) + 1
-        # print(f'\t{char}\t{x}\t{y}\t{presents_per_house[(x, y)]}\n')
+        # print(f'\t{char}\t{x}\t{y}\t{presents_per_house[(x, y)]}')
+
+    return presents_per_house
+
+
+def deliver_presents_with_robot(move_instructions: str):
+    """
+    Compute the number of presents assigned to each house as a function of the alternating moves of Santa and
+    Robo-Santa.
+
+    Moves are coded as a succession of characters: north (^), south (v), east (>), west (<).
+
+    :param move_instructions: string containing the move instructions
+    :return: a dictionary with the house coordinates (tuples) as key, and the number of presents per house as values
+    """
+    # print('Initializing')
+    # print(f'\tMove instructions:\t{move_instructions}')
+
+    # Initialize  parameters for Santa
+    instructions_santa = move_instructions[0:(len(move_instructions)):2]
+    # print(f'\tMove instructions for Santa:\t{instructions_santa}')
+
+    x_santa = 0
+    y_santa = 0
+
+    # Initialize parameters for Robo-Santa
+    instructions_robot = move_instructions[1:(len(move_instructions)):2]
+    # print(f'\tMove instructions for Robo-Santa:\t{instructions_robot}')
+    x_robot = 0
+    y_robot = 0
+
+    presents_per_house = {}
+    char = "."
+    presents_per_house[(0, 0)] = 1
+
+    # print("\nStart point")
+    # print(f'\t{char}\t{0}\t{0}\t{presents_per_house[(0, 0)]}')
+
+    # print("\nSantas' trajectory")
+    for char in instructions_santa:
+        x_santa, y_santa = one_move(x_santa, y_santa, char)
+        presents_per_house[(x_santa, y_santa)] = presents_per_house.get((x_santa, y_santa), 0) + 1
+        # print(f'\t{char}\t{x_santa}\t{y_santa}\t{presents_per_house[(x_santa, y_santa)]}')
+
+    # print("\nRobo-Santa's trajectory")
+    for char in instructions_robot:
+        x_robot, y_robot = one_move(x_robot, y_robot, char)
+        presents_per_house[(x_robot, y_robot)] = presents_per_house.get((x_robot, y_robot), 0) + 1
+        # print(f'\t{char}\t{x_robot}\t{y_robot}\t{presents_per_house[(x_robot, y_robot)]}')
 
     return presents_per_house
--- 2015/test_day_03_2015.py
+++ 2015/test_day_03_2015.py
@@ -1,16 +1,27 @@
 from unittest import TestCase
 
-from day_03_2015 import deliver_presents
+from day_03_2015 import deliver_presents, deliver_presents_with_robot
 
 
 class Test(TestCase):
     def test_deliver_presents(self):
-        test_instructions: str = '^^>v<'
-        result = deliver_presents(test_instructions)
-        assert len(result) == 5
-        assert result == {(0, 0): 1, (0, 1): 2, (0, 2): 1, (1, 2): 1, (1, 1): 1}
-        assert result[(0, 0)] == 1
-        assert result[(0, 2)] == 1
-        assert result[(1, 2)] == 1
-        assert result[(1, 1)] == 1
-        assert result[(0, 1)] == 2
+        print("Part one - Test 1")
+        self.assertEqual(5, len(deliver_presents('^^>v<')))
+        print("Part one - Test 2")
+        self.assertEqual(2, len(deliver_presents('^v^v^v^v^v')))
+        # test_instructions: str = '^^>v<'
+        # result = deliver_presents(test_instructions)
+        # assert len(result) == 5
+        # assert result == {(0, 0): 1, (0, 1): 2, (0, 2): 1, (1, 2): 1, (1, 1): 1}
+        # assert result[(0, 0)] == 1
+        # assert result[(0, 2)] == 1
+        # assert result[(1, 2)] == 1
+        # assert result[(1, 1)] == 1
+        # assert result[(0, 1)] == 2
+
+    def test_deliver_presents_with_robot(self):
+        print("Part two - Test 1")
+        self.assertEqual(3, len(deliver_presents_with_robot('^^>v<')))
+
+        print("Part two - Test 2")
+        self.assertEqual(11, len(deliver_presents_with_robot('^v^v^v^v^v')))
