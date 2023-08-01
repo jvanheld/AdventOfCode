f445445077d29eb92aa4b7e3c609db7e06c59163
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-01 18:56:47 +0200

    Implemented second rule (two identical sucessive letters) of day05 challenge

--- 2015/day_05_2015.py
+++ 2015/day_05_2015.py
@@ -31,18 +31,40 @@ def check_vowels(my_string: str):
     Check that my_string contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
 
     :param my_string: input string
-    :return: TRUE/FALSE
+    :return: True/False
     """
     vowels = "aeiou"
     vowel_counts = list(map(my_string.lower().count, vowels))
     vowel_counts = list(filter(lambda x: (x > 0), vowel_counts))  ## Only keep count of present vowels
-    print(f'\t{my_string}\t{vowel_counts}\t{len(vowel_counts)}')
     if (len(vowel_counts) < 3):
         rule1_check = False
     else:
         rule1_check = True
+    print(f'\t{my_string}\tRule 1\t{rule1_check}\t{len(vowel_counts)}')
     return rule1_check
 
+
+def check_double_letter(my_string: str):
+    """
+    Check that input string contains at least one letter that appears twice in a row, like xx, abcdde (dd),
+    or aabbccdd (aa, bb, cc, or dd).
+
+    :param my_string: input strung
+    :return: True/False
+    """
+    rule2_check = False
+    previous_letter = ''
+    for letter in my_string:
+        if letter == previous_letter:
+            rule2_check = True
+            break
+        previous_letter = letter
+    if rule2_check:
+        print(f'\t{my_string}\tRule 2\t{rule2_check}\t{previous_letter + letter}')
+    else:
+        print(f'\t{my_string}\tRule 2\t{rule2_check}')
+
+    return rule2_check
 
 def check_rules(my_string: str):
     """
@@ -57,6 +79,10 @@ def check_rules(my_string: str):
     :param my_string: input string on which the rules should be checked
     :return: a tuple with the list of non-matched rule numbers
     """
-
-    # Check rule 1
-    vowel_check = check_vowels(my_string)
+    print(f'\n{my_string}')
+    rule_checks = (check_vowels(my_string),
+                   check_double_letter(my_string))
+    count_checks = 0
+    for rule in rule_checks:
+        count_checks += int(rule)
+    return count_checks == 2
--- 2015/test_day_05_2015.py
+++ 2015/test_day_05_2015.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 
-from day_05_2015 import check_vowels
+from day_05_2015 import check_rules
 
 
 class Test(TestCase):
     def test_check_rules(self):
-        self.assertTrue(check_vowels('aueeiiioooouuuuu'))
-        self.assertTrue(check_vowels('ugknbfddgicrmopn'))
-        self.assertFalse(check_vowels('jchzalrnumimnmhp'))
+        self.assertTrue(check_rules('ugknbfddgicrmopn'))
+        self.assertFalse(check_rules('jchzalrnumimnmhp'))
+        self.assertFalse(check_rules('dvszwmarrgswjxmb'))
