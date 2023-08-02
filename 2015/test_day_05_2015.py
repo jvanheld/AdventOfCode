ad9e71452d4e6718c4a87b4c66ce672d6f3161ea
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-02 16:13:39 +0200

    added 2 strings to assess

--- 2015/test_day_05_2015.py
+++ 2015/test_day_05_2015.py
@@ -5,6 +5,19 @@ from day_05_2015 import check_rules
 
 class Test(TestCase):
     def test_check_rules(self):
+        # ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...),
+        # and none of the disallowed substrings.
         self.assertTrue(check_rules('ugknbfddgicrmopn'))
+
+        # aaa is nice because it has at least three vowels and a double letter, even though the letters used by
+        # different rules overlap.
+        self.assertTrue(check_rules('aaa'))
+
+        # jchzalrnumimnmhp is naughty because it has no double letter.
         self.assertFalse(check_rules('jchzalrnumimnmhp'))
+
+        # haegwjzuvuyypxyu is naughty because it contains the string xy.
+        self.assertFalse(check_rules('haegwjzuvuyypxyu'))
+
+        # dvszwmarrgswjxmb is naughty because it contains only one vowel.
         self.assertFalse(check_rules('dvszwmarrgswjxmb'))
