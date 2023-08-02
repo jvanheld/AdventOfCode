5015efa6c250d282769985cdbf82de0759b54a1b
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-02 16:13:05 +0200

    Implemented third rule + fixed rule 1 (initial version required 3 distinct vowels, whereas the actual requirement is just to have 3 vowels, which can be different or identical).

--- 2015/day_05_2015.py
+++ 2015/day_05_2015.py
@@ -35,12 +35,13 @@ def check_vowels(my_string: str):
     """
     vowels = "aeiou"
     vowel_counts = list(map(my_string.lower().count, vowels))
-    vowel_counts = list(filter(lambda x: (x > 0), vowel_counts))  ## Only keep count of present vowels
-    if (len(vowel_counts) < 3):
-        rule1_check = False
-    else:
-        rule1_check = True
-    print(f'\t{my_string}\tRule 1\t{rule1_check}\t{len(vowel_counts)}')
+    rule1_check = sum(vowel_counts) >= 3
+    # vowel_counts = list(filter(lambda x: (x > 0), vowel_counts))  ## Only keep count of present vowels
+    # if (len(vowel_counts) < 3):
+    #     rule1_check = False
+    # else:
+    #     rule1_check = True
+    print(f'\t{my_string}\tRule 1\t{rule1_check}\t{sum(vowel_counts)}')
     return rule1_check
 
 
@@ -66,6 +67,20 @@ def check_double_letter(my_string: str):
 
     return rule2_check
 
+
+def check_fobidden_strings(my_string: str, forbidden_strings=['ab', 'cd', 'pq', 'xy']):
+    """
+    Check that the input string does not contain forbidden strings (provided as a list).
+
+    :param my_string: input string
+    :param forbidden_strings: a list of forbudden strings
+    :return: True/False
+    """
+    forbidden_counts = list(map(my_string.lower().count, forbidden_strings))
+    print(f'\t{my_string}\tRule 3\t{sum(forbidden_counts) == 0}\t{sum(forbidden_counts)}\t{forbidden_counts}')
+    return sum(forbidden_counts) == 0
+
+
 def check_rules(my_string: str):
     """
     Check the rules of a given input string.
@@ -81,8 +96,8 @@ def check_rules(my_string: str):
     """
     print(f'\n{my_string}')
     rule_checks = (check_vowels(my_string),
-                   check_double_letter(my_string))
-    count_checks = 0
-    for rule in rule_checks:
-        count_checks += int(rule)
-    return count_checks == 2
+                   check_double_letter(my_string),
+                   check_fobidden_strings(my_string))
+    count_checks = sum(rule_checks)
+    print(f'\tValid rules: {count_checks}')
+    return count_checks == 3
