fbcda04e90ca12d9dde34feaac878a9658e765b9
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-03 23:44:52 +0200

    Uncommented day04

--- 2015/main.py
+++ 2015/main.py
@@ -81,7 +81,7 @@ def main():
     cProfile.run('day01()')
     cProfile.run('day02()')
     cProfile.run('day03()')
-    # cProfile.run('day04()')
+    cProfile.run('day04()')
     cProfile.run('day05()')
     print("\n\nAll days done\n\n")
 
