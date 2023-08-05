2390625bfa00f1de6382714b5e77e775c8513656
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-05 20:45:13 +0200

    fixed suppression of import lines by PyCharm

--- 2015/main.py
+++ 2015/main.py
@@ -6,6 +6,12 @@ https://adventofcode.com/2015
 """
 
 import cProfile
+from day_01_2015 import day01
+from day_02_2015 import day02
+from day_03_2015 import day03
+from day_04_2015 import day04
+from day_05_2015 import day05
+from day_06_2015 import day06
 
 
 def main():
