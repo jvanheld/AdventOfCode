f16b7926246d981d2ef3300695c40d9f3d09e281
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-05 23:38:29 +0200

    Added data file for day 07

--- 2015/main.py
+++ 2015/main.py
@@ -6,12 +6,6 @@ https://adventofcode.com/2015
 """
 
 import cProfile
-from day_01_2015 import day01
-from day_02_2015 import day02
-from day_03_2015 import day03
-from day_04_2015 import day04
-from day_05_2015 import day05
-from day_06_2015 import day06
 
 
 def main():
@@ -21,6 +15,7 @@ def main():
     # cProfile.run('day04()')
     cProfile.run('day05()')
     cProfile.run('day06()')
+    cProfile.run('day07()')
     print("\n\nAll days done\n\n")
 
 
