70a4eceae70c66be92849277c5bad98febb6eb78
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-07 06:28:35 +0200

    restored imports and computation of all the days from 01 to 07

--- 2015/main.py
+++ 2015/main.py
@@ -5,6 +5,13 @@ https://adventofcode.com/2015
 
 """
 
+import cProfile
+from day_01_2015 import day01
+from day_02_2015 import day02
+from day_03_2015 import day03
+from day_04_2015 import day04
+from day_05_2015 import day05
+from day_06_2015 import day06
 from day_07_2015 import day07
 
 
@@ -15,16 +22,15 @@ def main():
     # day04()
     # day05()
     # day06()
-    day07()
-    #    cProfile.run('day01()')
-    #    cProfile.run('day02()')
-    #    cProfile.run('day03()')
-    #    # cProfile.run('day04()')
-    #    cProfile.run('day05()')
-    #    cProfile.run('day06()')
-    #    cProfile.run('day07()')
+    # day07()
+    cProfile.run('day01()')
+    cProfile.run('day02()')
+    cProfile.run('day03()')
+    cProfile.run('day04()')
+    cProfile.run('day05()')
+    cProfile.run('day06()')
+    cProfile.run('day07()')
     print("\n\nAll days done\n\n")
-
 
 if __name__ == '__main__':
     main()
