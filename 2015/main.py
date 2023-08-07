5b299af7fe3ff30fb6145a1ae98bae7cd8a899e0
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-07 08:59:59 +0200

    Suppressed cProfiler

--- 2015/main.py
+++ 2015/main.py
@@ -14,23 +14,17 @@ from day_05_2015 import day05
 from day_06_2015 import day06
 from day_07_2015 import day07
 
-
 def main():
-    # day01()
-    # day02()
-    # day03()
-    # day04()
-    # day05()
-    # day06()
-    # day07()
-    cProfile.run('day01()')
-    cProfile.run('day02()')
-    cProfile.run('day03()')
-    cProfile.run('day04()')
-    cProfile.run('day05()')
-    cProfile.run('day06()')
-    cProfile.run('day07()')
-    print("\n\nAll days done\n\n")
+    day01()
+    day02()
+    day03()
+    day04()
+    day05()
+    day06()
+    day07()
+
+
+print("\n\nAll days done\n\n")
 
 if __name__ == '__main__':
     main()
