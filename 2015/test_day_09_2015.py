0107f9c50fc90e9ce01118471aeffc317670974f
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-17 11:53:14 +0200

    Fixed an error in one test

--- 2015/test_day_09_2015.py
+++ 2015/test_day_09_2015.py
@@ -6,5 +6,5 @@ from day_09_2015 import permutations
 class Test(TestCase):
     def test_permutations(self):
         self.assertEqual([], permutations([]))
-        self.assertEqual(["Bruxelles"], permutations(["Bruxelles"]))
+        self.assertEqual([["Bruxelles"]], permutations(["Bruxelles"]))
         self.assertEqual([['a', 'b'], ['b', 'a']], permutations(['a', 'b']))
