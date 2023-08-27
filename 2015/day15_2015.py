c18956195b3626b1a078eaa5aa561c2876f4d694
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2023-08-28 01:19:40 +0200

    Implemented day15 Part One + unit tests

--- 2015/day15_2015.py
+++ 2015/day15_2015.py
@@ -37,6 +37,19 @@ produced a negative total, it would have instead become zero, causing the whole 
 Given the ingredients in your kitchen and their properties, what is the total score of the highest-scoring cookie you
 can make?
 
+--- Part Two ---
+
+Your cookie recipe becomes wildly popular! Someone asks if you can make another recipe that has exactly 500 calories
+per cookie (so they can use it as a meal replacement). Keep the rest of your award-winning process the same (100
+teaspoons, same ingredients, same scoring system).
+
+For example, given the ingredients above, if you had instead selected 40 teaspoons of butterscotch and 60 teaspoons
+of cinnamon (which still adds to 100), the total calorie count would be 40*8 + 60*3 = 500. The total score would go
+down, though: only 57600000, the best you can do in such trying circumstances.
+
+Given the ingredients in your kitchen and their properties, what is the total score of the highest-scoring cookie you
+can make with a calorie total of 500?
+
 
 """
 import os
@@ -77,9 +90,52 @@ def read_data(infile: str):
     return ingredients, features, values
 
 
+def all_partitions(n=100, groups=4):
+    partitions = list()
+    # print(f"all_partitions({n}, {groups})")
+    if groups == 1:
+        partitions = [[n]]
+        # print(f"One group\tt{type(partitions)}\t{partitions}")
+    else:
+        for i in range(0, n + 1):
+            # print(f"{n}\t{groups}\t{i}")
+            sub_partitions = all_partitions(n - i, groups - 1)
+            for partition in sub_partitions:
+                # print(f"concat\t{i}\t{partition}")
+                partitions.append([i] + partition)
+    return partitions
+
+
+def food_value(percentages, values):
+    sum_per_feature = percentages[0] * values[0]
+    # print(f"{percentages[0]}\t{values[0]}\t{sum_per_feature}")
+    for i in range(1, len(percentages)):
+        sum_per_feature += percentages[i] * values[i]
+        # print(f"{percentages[i]}\t{values[i]}\t{sum_per_feature}")
+    sum_per_feature = np.maximum(sum_per_feature, [0] * len(sum_per_feature))
+    return np.prod(sum_per_feature)
+
+
+
 def day15():
     ingredients, features, values = read_data('2015/data/data_2015_day15.txt')
-    print(values)
+    # print(values)
+    best_value = 0
+    values_no_cal = values[0:len(ingredients), 0:4]  # Values without the calories
+    print(values_no_cal)
+    for i, percentages in enumerate(all_partitions(n=100, groups=len(ingredients))):
+        # print(percentages)
+        current_value = max(best_value, food_value(percentages, values_no_cal))
+        if current_value > best_value:
+            best_value = current_value
+            best_percentages = percentages
+    print(f"\n\nDay 15 - Part One")
+    print(f"\tPartitions: {i + 1}")
+    print(f"\tBest value: {best_value}")
+    print(f"\tBest percentages: {best_percentages}")
+
+
+
 
 if __name__ == '__main__':
     os.chdir('..')
